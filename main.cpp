a323f80c3634c55e443286de90ba4e1b7fc35318
Arne Osterthun <arne.osterthun@uni-weimar.de>
2015-12-03 19:47:49 +0100

    Some Stuff is working

--- main.cpp
+++ main.cpp
@@ -12,16 +12,16 @@ int main(int argc, char* argv[])
 {	
 	std::string conversion = argv[1];
 	std::string value = argv[2];
-	ConverterFactory  converter;
+	ConverterFactory* converter = ConverterFactory::getInstance();
 
-	converter.registerClass("dollarToEuroConverter", new dollarToEuroConverter);
-	converter.registerClass("euroToDollarConverter", new euroToDollarConverter);
-	converter.registerClass("euroToDanishKroneConverter", new euroToDanishKroneConverter);
-	converter.registerClass("centimeterToInchesConverter", new centimeterToInchesConverter);
-	converter.registerClass("kilometerToMilesConverter", new kilometerToMilesConverter);
+	converter->registerClass("dollarToEuroConverter", new dollarToEuroConverter);
+	converter->registerClass("euroToDollarConverter", new euroToDollarConverter);
+	converter->registerClass("euroToDanishKroneConverter", new euroToDanishKroneConverter);
+	converter->registerClass("centimeterToInchesConverter", new centimeterToInchesConverter);
+	converter->registerClass("kilometerToMilesConverter", new kilometerToMilesConverter);
 
 
-	auto myConverter = converter.create(conversion);
+	auto myConverter = converter->create(conversion);
 	double output = myConverter->convert(std::stod(value));
 	std::cout << myConverter->toString() << " has converted "<< value << " to " << output <<" !"<<std::endl;
 	return 0;
