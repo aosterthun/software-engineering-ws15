2c56e927cbf6ee950c729f35af6b3c3805243932
Arne Osterthun <arne.osterthun@uni-weimar.de>
2015-12-03 19:29:22 +0100

    SEGMENTATION FUCK

--- main.cpp
+++ main.cpp
@@ -13,38 +13,15 @@ int main(int argc, char* argv[])
 	std::string conversion = argv[1];
 	std::string value = argv[2];
 	ConverterFactory::registerClass("dollarToEuroConverter", new dollarToEuroConverter);
+	ConverterFactory::registerClass("euroToDollarConverter", new euroToDollarConverter);
+	ConverterFactory::registerClass("euroToDanishKroneConverter", new euroToDanishKroneConverter);
+	ConverterFactory::registerClass("centimeterToInchesConverter", new centimeterToInchesConverter);
+	ConverterFactory::registerClass("kilometerToMilesConverter", new kilometerToMilesConverter);
 
-	 //std::cout << conversion;
-	 if (conversion == "dollarToEuroConverter")
-	 {
-		auto myConverter = ConverterFactory::create("dollarToEuroConverter");
-		double output = myConverter->convert(std::stod(value));
-		std::cout << myConverter->toString() << " has converted "<< value << " to " << output <<" !"<<std::endl;
-	 } 
-	 else if (conversion == "euroToDollarConverter")
-	 {
-		 auto myConverter = std::make_shared<euroToDollarConverter>();
-		 double output = myConverter->convert(std::stod(value));
-		 std::cout << myConverter->toString() << " has converted "<< value << " to " << output <<" !"<<std::endl;
-	 }
-	 else if (conversion == "euroToDanishKroneConverter")
-	 {
-		 auto myConverter = std::make_shared<euroToDanishKroneConverter>();
-		 double output = myConverter->convert(std::stod(value));
-		 std::cout << myConverter->toString() << " has converted "<< value << " to " << output <<" !"<<std::endl;
-	 }
-	 else if (conversion == "centimeterToInchesConverter")
-	 {
-	   auto myConverter = std::make_shared<centimeterToInchesConverter>();
-	   double output = myConverter->convert(std::stod(value));
-	   std::cout << myConverter->toString() << " has converted "<< value << " to " << output <<" !"<<std::endl;
-	 }
-	 else if (conversion == "kilometerToMilesConverter")
-	 {
-	   auto myConverter = std::make_shared<kilometerToMilesConverter>();
-	   double output = myConverter->convert(std::stod(value));
-	   std::cout << myConverter->toString() << " has converted "<< value << " to " << output <<" !"<<std::endl;
-	 }
+
+	auto myConverter = ConverterFactory::create(conversion);
+	double output = myConverter->convert(std::stod(value));
+	std::cout << myConverter->toString() << " has converted "<< value << " to " << output <<" !"<<std::endl;
 	return 0;
 }
 
